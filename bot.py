286cc95910a4c04ea42ee2e800feb8620502b282
Arka Saha <i.am.arka.saha@gmail.com>
2021-05-19 17:23:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,5 +18,26 @@ async def hi(ctx):
 async def say(ctx,*,text):
     await ctx.send(f"{ctx.author} just said: {text}")
 
+    
+@client.command()
+async def info(ctx, member: discord.Member):
+    member_type = "HUMAN"
+
+    if member.bot == True:
+        member_type = "BOT"
+    elif member.bot == False:
+        member_type = " HUMAN"
+
+    embed = discord.Embed(title=member.display_name, color=discord.Color.dark_teal())
+    embed.add_field(name="ACCOUNT ID", value=member.id, inline=False)
+    embed.add_field(name="ACCOUNT CREATED AT", value=member.created_at, inline=False)
+    embed.add_field(name="JOINED SERVER AT", value=member.joined_at, inline=False)
+    embed.add_field(name="STATUS", value=str(member.raw_status).title(), inline=False)
+    embed.add_field(name="TYPE", value=member_type, inline=False)
+    embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(icon_url=ctx.author.avatar_url, text=f"Requested By {ctx.author}")
+
+    await ctx.send(embed=embed)
+
 
 client.run("ODIxNjUwMzMzMDk1NjkwMjgw.YFGzgA.H5JKzahV1eAhgV0zW58ummEVpv0")  #paste Bot Token Here
